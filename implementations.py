274ad2195e49c5118b242b5ad5abe9d0954a7216
haraldhob <64754418+haraldhob@users.noreply.github.com>
2023-10-30 11:47:25 +0100

    Update implementations.py

--- implementations.py
+++ implementations.py
@@ -4,21 +4,31 @@ import matplotlib.pyplot as plt
 
 
 def least_squares(y, tx):
-    """
-    Calculate the least squares solution.
+    """Calculate the least squares solution.
+       returns mse, and optimal weights.
 
     Args:
-        y: shape=(N, 1)
-        tx: shape=(N, D)
+        y: numpy array of shape (N,), N is the number of samples.
+        tx: numpy array of shape (N,D), D is the number of features.
 
     Returns:
-        w: shape=(D, 1)
-        mse: scalar
-    """
-    w = np.linalg.lstsq(tx.T @ tx, tx.T @ y, rcond=None)[0]
-    res = y - tx @ w
-    mse = res.T @ res / (2 * len(y))
+        w: optimal weights, numpy array of shape(D,), D is the number of features.
+        mse: scalar.
 
+    >>> least_squares(np.array([0.1,0.2]), np.array([[2.3, 3.2], [1., 0.1]]))
+    (array([ 0.21212121, -0.12121212]), 8.666684749742561e-33)
+    """
+    # Solve normal equation and get first result
+    equation = tx.T @ tx, tx.T @ y
+    ws = np.linalg.lstsq(
+            equation,
+            rcond=None
+    )
+    w = ws[0]
+    # Calculate residuals
+    res = y - tx @ w
+    # Calculate the Mean Squared Error
+    mse = (res.T @ res)/(2*len(y))
     return w, mse
 
 
@@ -60,7 +70,7 @@ def calculate_loss(y, tx, w):
     assert tx.shape[1] == w.shape[0]
 
     N = y.shape[0]
-    loss = -1 / N * np.sum(y * np.log(sigmoid(tx @ w)) + (1 - y) * np.log(1 - sigmoid(tx @ w)))
+    loss = (-1 / N) * np.sum(y*np.log(sigmoid(tx @ w)) + (1-y)*np.log(1-sigmoid(tx @ w)))
     return loss
 
 
@@ -85,7 +95,7 @@ def calculate_gradient(y, tx, w):
            [ 0.51712843]])
     """
     N = y.shape[0]
-    return 1 / N * tx.T @ (sigmoid(tx @ w) - y)
+    return (1 / N) * tx.T @ (sigmoid(tx @ w)-y)
 
 
 def learning_by_gradient_descent(y, tx, w, gamma):
@@ -120,52 +130,45 @@ def learning_by_gradient_descent(y, tx, w, gamma):
 
 
 def logistic_regression(y, tx, initial_w=None, max_iter=10000, gamma=0.1, threshold=1e-12):
+    """return the loss, gradient of the loss, and hessian of the loss.
+
+    Args:
+        y:  shape=(N, 1)
+        tx: shape=(N, D)
+        initial_w:  shape=(D, 1)
+        max_iter: int and maximum amount of iterations
+        gamma: float and learning rate
+        threshold: float and threshold as absolute diff to stop
+
+    Returns:
+        loss: scalar number
+        gradient: shape=(D, 1)
+
+    >>> y = np.c_[[0., 1.]]
+    >>> tx = np.arange(6).reshape(2, 3)
+    >>> w = np.array([[0.1], [0.2], [0.3]])
+    >>> gradient, loss = logistic_regression(y, tx, w)
+    >>> round(loss, 8)
+    0.62137268
+    >>> gradient
+    (array([[-0.10370763],
+           [ 0.2067104 ],
+           [ 0.51712843]]))
     """
-        Do logistic regression with the specified parameters until it converges under threshold.
-
-        Args:
-            y:  shape=(N, 1)
-            tx: shape=(N, D)
-            initial_w:  shape=(D, 1)
-            max_iter: int
-            gamma: float
-            threshold: float
-
-        Returns:
-            w: shape=(D, 1)
-            loss: scalar number
-
-        >>> y = np.c_[[0., 1.]]
-        >>> tx = np.arange(6).reshape(2, 3)
-        >>> w = np.array([[0.1], [0.2], [0.3]])
-        >>> gamma = 0.1
-        >>> w, loss = learning_by_gradient_descent(y, tx, w, gamma)
-        >>> round(loss, 8)
-        0.62137268
-        >>> w
-        array([[0.11037076],
-               [0.17932896],
-               [0.24828716]])
-        """
     losses = []
-
     w = np.zeros((tx.shape[1], 1)) if initial_w is None else initial_w
-
-    # start the logistic regression
+    # Iterate over max iterations. Stop when we get convergence under threshold.
     for iter in range(max_iter):
-        # get loss and update w.
+        # get loss and w.
         loss, w = learning_by_gradient_descent(y, tx, w, gamma)
-        # log info
-        if iter % 100 == 0:
-            print("Current iteration={i}, loss={l}".format(i=iter, l=loss))
-        # converge criterion
+        if (iter % 10) == 0:
+            print("Current iteration={%s}, loss={%s}" % (iter, loss))
+        # converge check
         losses.append(loss)
-        if len(losses) > 1 and np.abs(losses[-1] - losses[-2]) < threshold:
+        if len(losses) > 1 and (np.abs(losses[-1] - losses[-2]) < threshold):
             break
 
     loss = calculate_loss(y, tx, w)
-    print("loss={l}".format(l=loss))
-
-    # plot losses
+    print("loss={%s}" % loss)
     plt.plot(losses)
     return w, loss
